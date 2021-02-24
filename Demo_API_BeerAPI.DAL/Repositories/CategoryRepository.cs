60b2a3b06310490c13108167f1d31ad1893a9fb6
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 15:22:52 +0100

    Add DAL for Category

---
+++ Demo_API_BeerAPI.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using Demo_API_BeerAPI.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Database;
+
+namespace Demo_API_BeerAPI.DAL.Repositories
+{
+    public class CategoryRepository : RepositoryBase<int, CategoryEntity>
+    {
+        public CategoryRepository() : base("Category", "Id_Category") 
+        { }
+
+
+        public override int Insert(CategoryEntity entity)
+        {
+            QueryDB query = new QueryDB("InsertCategory", true);
+            query.AddParametre("@Name", entity.Name);
+
+            return (int) ConnectDB.ExecuteScalar(query);
+        }
+
+        public override bool Update(int id, CategoryEntity entity)
+        {
+            QueryDB query = new QueryDB("UpdateCategory", true);
+            query.AddParametre("@Id_Category", id);
+            query.AddParametre("@Name", entity.Name);
+
+            try
+            {
+                return ConnectDB.ExecuteNonQuery(query) == 1;
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+        }
+
+        protected override CategoryEntity ConvertDataReaderToEntity(IDataReader reader)
+        {
+            return new CategoryEntity()
+            {
+                Id = Convert.ToInt32(reader["Id_Category"]),
+                Name = reader["Name"].ToString()
+            };
+        }
+    }
+}
