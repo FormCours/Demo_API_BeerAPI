c5b8a20da3b7a34bcdd2a23a7998a7ad549930e4
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 11:30:54 +0100

    Add DAL for Brewery

---
+++ Demo_API_BeerAPI.DAL/Repositories/BreweryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Demo_API_BeerAPI.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Database;
+
+namespace Demo_API_BeerAPI.DAL.Repositories
+{
+    public class BreweryRepository : RepositoryBase<int, BreweryEntity>
+    {
+        public BreweryRepository() : base("Brewery", "Id_Brewery") 
+        { }
+
+
+        public override int Insert(BreweryEntity entity)
+        {
+            QueryDB query = new QueryDB("InsertBrewery", true);
+            query.AddParametre("@Name", entity.Name);
+            query.AddParametre("@Headquarter", entity.Headquarter);
+            query.AddParametre("@Country", entity.Country);
+
+            return (int) ConnectDB.ExecuteScalar(query);
+        }
+
+        public override bool Update(int id, BreweryEntity entity)
+        {
+            QueryDB query = new QueryDB("UpdateBrewery", true);
+            query.AddParametre("@Id_Brewery", entity.Id);
+            query.AddParametre("@Name", entity.Name);
+            query.AddParametre("@Headquarter", entity.Headquarter);
+            query.AddParametre("@Country", entity.Country);
+
+            try
+            {
+                ConnectDB.ExecuteNonQuery(query);
+                return true;
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+        }
+
+        protected override BreweryEntity ConvertDataReaderToEntity(IDataReader reader)
+        {
+            return new BreweryEntity()
+            {
+                Id = Convert.ToInt32(reader["Id_Brewery"]),
+                Name = reader["Name"].ToString(),
+                Headquarter = (reader["Headquarter"] is DBNull) ? null : reader["Headquarter"].ToString(),
+                Country = (reader["Country"] is DBNull) ? null : reader["Country"].ToString()
+            };
+        }
+    }
+}
---
+++ Demo_API_BeerAPI.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Database;
+using Toolbox.Repository;
+
+namespace Demo_API_BeerAPI.DAL.Repositories
+{
+    public abstract class RepositoryBase<TKey, TEntity> : IRepository<TKey, TEntity>
+        where TEntity : IEntity<TKey>
+    {
+        protected ConnectDB ConnectDB { get; }
+
+        protected string TableName { get; }
+        protected string IdName { get; }
+
+        public RepositoryBase(string tableName, string idName = "Id")
+        {
+            ConnectDB = new ConnectDB(ConfigurationManager.ConnectionStrings["db-source"].ConnectionString);
+
+            TableName = tableName;
+            IdName = idName;
+        }
+
+        protected abstract TEntity ConvertDataReaderToEntity(IDataReader reader);
+
+        public virtual TEntity Get(TKey id)
+        {
+            QueryDB query = new QueryDB($"SELECT * FROM [{TableName}] WHERE [{IdName}] = @Id");
+            query.AddParametre("@Id", id);
+
+            return ConnectDB.ExecuteReader(query, ConvertDataReaderToEntity).SingleOrDefault();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            QueryDB query = new QueryDB($"SELECT * FROM [{TableName}]");
+
+            return ConnectDB.ExecuteReader(query, ConvertDataReaderToEntity);
+        }
+
+        public abstract TKey Insert(TEntity entity);
+
+        public abstract bool Update(TKey id, TEntity entity);
+
+        public virtual bool Delete(TKey id)
+        {
+            QueryDB query = new QueryDB($"DELETE FROM [{TableName}] WHERE [{IdName}] = @Id");
+            query.AddParametre("@Id", id);
+
+            return ConnectDB.ExecuteNonQuery(query) == 1;
+        }
+    }
+}
