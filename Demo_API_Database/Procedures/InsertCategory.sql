672279b4a294ded5cbb709bcaaef344bad5bfa23
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 14:36:46 +0100

    Add Stored procedure for Category

---
+++ Demo_API_Database/Procedures/InsertCategory.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[InsertCategory]
+	@Name VARCHAR(50)
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   INSERT INTO [Category] (Name)
+    OUTPUT inserted.Id_Category
+    VALUES (@Name);
+END
---
+++ Demo_API_Database/Procedures/UpdateCategory.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateCategory]
+    @Id_Category INT,
+	@Name VARCHAR(50)
+
+AS
+BEGIN
+   IF NOT EXISTS (SELECT * FROM [Category] WHERE Id_Category = @Id_Category)
+	  THROW 50042, 'Category not exists', 1;
+
+   IF LTRIM(RTRIM(@Name)) LIKE ''
+	  THROW 51042, 'Category name is empty', 1;
+   
+   UPDATE [Category] 
+    SET [Name] = @Name
+   WHERE [Id_Category] = @Id_Category
+END
