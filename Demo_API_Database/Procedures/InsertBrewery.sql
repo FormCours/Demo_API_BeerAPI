7fc56170afb994996caeda46c12c0a59323342c8
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 10:03:33 +0100

    Add Stored procedure for Brewery

---
+++ Demo_API_Database/Procedures/InsertBrewery.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[InsertBrewery]
+	@Name VARCHAR(50),
+	@Headquarter VARCHAR(200) = NULL,
+	@Country VARCHAR(50) = NULL
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   INSERT INTO [Brewery] (Name, Headquarter, Country)
+    OUTPUT inserted.Id_Brewery
+    VALUES (@Name, @Headquarter, @Country);
+END
---
+++ Demo_API_Database/Procedures/UpdateBrewery.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[UpdateBrewery]
+    @Id_Brewery INT,
+	@Name VARCHAR(50),
+	@Headquarter VARCHAR(200) = NULL,
+	@Country VARCHAR(50) = NULL
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   IF NOT EXISTS (SELECT * FROM [Brewery] WHERE Id_Brewery = @Id_Brewery)
+	  THROW 50042, 'Brewery not exists', 1;
+
+   IF LTRIM(RTRIM(@Name)) LIKE ''
+	  THROW 51042, 'Brewery name is empty', 1;
+   
+   UPDATE [Brewery] 
+    SET [Name] = @Name,
+	    [Headquarter] = @Headquarter,
+	    [Country] = @Country
+   WHERE [Id_Brewery] = @Id_Brewery
+END
