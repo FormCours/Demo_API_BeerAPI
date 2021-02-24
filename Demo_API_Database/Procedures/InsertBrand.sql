56642322e2d02f8dd4349ecccdc8be66a556fed4
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 14:36:38 +0100

    Add Stored procedure for Brand

---
+++ Demo_API_Database/Procedures/InsertBrand.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[InsertBrand]
+	@Name VARCHAR(50),
+	@Country VARCHAR(50) = NULL,
+	@Creation_Year INT = NULL
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   INSERT INTO [Brand] (Name, Country, Creation_Year)
+    OUTPUT inserted.Id_Brand
+    VALUES (@Name, @Country, @Creation_Year);
+END
---
+++ Demo_API_Database/Procedures/UpdateBrand.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[UpdateBrand]
+    @Id_Brand INT,
+	@Name VARCHAR(50),
+	@Country VARCHAR(50) = NULL,
+	@Creation_Year INT = NULL
+AS
+BEGIN
+   IF NOT EXISTS (SELECT * FROM [Brand] WHERE Id_Brand = @Id_Brand)
+	  THROW 50042, 'Brand not exists', 1;
+
+   IF LTRIM(RTRIM(@Name)) LIKE ''
+	  THROW 51042, 'Brand name is empty', 1;
+   
+   UPDATE [Brand] 
+    SET [Name] = @Name,
+		[Country] = @Country,
+		[Creation_Year] = @Country
+   WHERE [Id_Brand] = @Id_Brand
+END
