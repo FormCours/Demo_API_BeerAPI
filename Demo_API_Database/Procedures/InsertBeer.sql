1c30be7c21c2080c744a41cef93ee74e3f75eb54
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-24 15:30:12 +0100

    Add Stored procedure for Beer

---
+++ Demo_API_Database/Procedures/InsertBeer.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[InsertBeer]
+	@Name VARCHAR(50),
+	@Id_Brewery INT,
+	@Id_Brand INT = NULL,
+	@Color VARCHAR(50) = NULL,
+	@Degree decimal(18,0) = NULL
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   INSERT INTO [Beer] (Name, Color, Degree, Id_Brewery, Id_Brand)
+    OUTPUT inserted.Id_Beer
+	VALUES (@Name, @Color, @Degree, @Id_Brewery, @Id_Brand)
+END
---
+++ Demo_API_Database/Procedures/UpdateBeer.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[UpdateBeer]
+    @Id_Beer INT,
+	@Name VARCHAR(50),
+	@Id_Brewery INT,
+	@Id_Brand INT = NULL,
+	@Color VARCHAR(50) = NULL,
+	@Degree decimal(18,0) = NULL
+AS
+BEGIN  
+   UPDATE [Beer] 
+    SET [Name] = @Name,
+	   [Name] = @Name,
+	   [Id_Brewery] = @Id_Brewery,
+	   [Id_Brand] = @Id_Brand,
+	   [Color] = @Color,
+	   [Degree] = @Degree
+   WHERE [Id_Beer] = @Id_Beer
+END
