a6de317bf0c09a770e70eb321c0cd9c7cc8201c9
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-26 09:24:04 +0100

    Add model for collection response API

---
+++ Demo_API_BeerAPI/ModelsAPI/CollectionResponseAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_API_Intro.ModelsAPI
+{
+    public class CollectionResponseAPI
+    {
+        public int Total { get; set; }
+        public int NbResult { get { return Results.Count(); } }
+        public IEnumerable<object> Results { get; set; }
+
+        public CollectionResponseAPI(int total, IEnumerable<object> data) 
+        {
+            this.Total = total;
+            this.Results = data;
+        }
+    }
+}
