a26111f0bc6d2fd22e81e387bd56a47b0afcb45d
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-02-26 09:23:22 +0100

    Add model for pagination parameter

---
+++ Demo_API_BeerAPI/ModelsAPI/PaginationParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_API_Intro.ModelsAPI
+{
+    public class PaginationParameter
+    {
+        public int Offset { get; set; }
+        public int Limit { get; set; }
+
+        public PaginationParameter()
+        {
+            Offset = 0;
+            Limit = 10;
+        }
+    }
+}
