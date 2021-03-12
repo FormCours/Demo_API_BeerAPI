3bed6e073dac5fd4f31d8d502716d0b29d77c03b
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-03-12 14:22:51 +0100

    Add DAL for Member

---
+++ Demo_API_BeerAPI.DAL/Entities/MemberEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Repository;
+
+namespace Demo_API_BeerAPI.DAL.Entities
+{
+    public class MemberEntity : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Demo_API_BeerAPI.DAL/Repositories/MemberRepository.cs
@@ -0,0 +1,53 @@
+﻿using Demo_API_BeerAPI.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Database;
+
+namespace Demo_API_BeerAPI.DAL.Repositories
+{
+    public class MemberRepository : RepositoryBase<int, MemberEntity>
+    {
+        public MemberRepository() : base("V_Member", "Id_Member")
+        { }
+
+        public override int Insert(MemberEntity entity)
+        {
+            QueryDB query = new QueryDB("InsertMember", true);
+            query.AddParametre("@Username", entity.Username);
+            query.AddParametre("@Email", entity.Email);
+            query.AddParametre("@Password", entity.Password);
+
+            return (int) ConnectDB.ExecuteScalar(query);
+        }
+
+        public MemberEntity GetByCredential(string email, string password)
+        {
+            QueryDB query = new QueryDB("RetrieveMember", true);
+            query.AddParametre("@Email", email);
+            query.AddParametre("@Password", password);
+
+            return ConnectDB.ExecuteReader(query, ConvertDataReaderToEntity).SingleOrDefault();
+        }
+
+        public override bool Update(int id, MemberEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override MemberEntity ConvertDataReaderToEntity(IDataReader reader)
+        {
+            return new MemberEntity()
+            {
+                Id = Convert.ToInt32(reader["Id_Member"]),
+                Username = reader["Username"].ToString(),
+                Email = reader["Email"].ToString(),
+                Password = null,
+                Role = reader["Role"].ToString()
+            };
+        }
+    }
+}
